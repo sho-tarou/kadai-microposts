bab63a68c106f6030b9c2f0503266d102d1e23ae
shouma.itezono <zooonshou@gmail.com>
2021-03-12 05:04:07 +0000

    follow, unfollow

---
+++ app/Http/Controllers/UserFollowController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserFollowController extends Controller
+{
+    /**
+     * ユーザをフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function store($id)
+    {
+        // 認証済みユーザがidのユーザをフォローする
+        \Auth::user()->follow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+    
+    /**
+     * ユーザをアンフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // 認証済みユーザがidのユーザをアンフォローする
+        \Auth::user()->unfollow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+}
